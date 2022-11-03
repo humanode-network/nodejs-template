e516ec8d6a15dc4ef5af87aa5298d1633e73862f
MOZGIII <mike-n@narod.ru>
2022-11-03 16:41:15 +0400

    Add the integration code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world");
+// Required imports
+import { ApiPromise, WsProvider } from "@polkadot/api";
+
+const rpcWsUrl = process.env.RPC_WS_URL || "ws://127.0.0.1:9944";
+
+// Initialise the provider to connect to the local node.
+const provider = new WsProvider(rpcWsUrl);
+
+// Create the API and wait until ready.
+const api = await ApiPromise.create({ provider });
+
+// Retrieve the chain & node information information via rpc calls
+const [chain, nodeName, nodeVersion] = await Promise.all([
+  api.rpc.system.chain(),
+  api.rpc.system.name(),
+  api.rpc.system.version(),
+]);
+
+console.log(
+  `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
+);
